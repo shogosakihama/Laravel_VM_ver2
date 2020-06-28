ccd937104e7d0e11b017a21510bd8cef0489c84f
Shogo Sakihama <goshalways@gmail.com>
2020-06-29 01:22:23 +0900

    jwt達成

--- routes/api.php
+++ routes/api.php
@@ -18,6 +18,12 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::post('login', 'AuthController@login');
+ 
+Route::group(['middleware' => 'auth:api'], function () {
+    Route::get('/me', 'AuthController@me');
+    Route::post('/logout', 'AuthController@logout');
+});
 
 Route::get('/', 'AxiosPostController@index');
 
