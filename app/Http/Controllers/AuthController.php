51019f734b67c6e7d92e0839c9284fb515ddb634
Shogo Sakihama <goshalways@gmail.com>
2020-06-29 23:48:57 +0900

    やっぱりNuxtでやりたいから、これからもどす

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -7,66 +7,68 @@ use App\Http\Controllers\Controller;
 
 class AuthController extends Controller
 {
-    /**
-     * Create a new AuthController instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        $this->middleware('auth:api', ['except' => ['login']]);
+  /**
+   * Create a new AuthController instance.
+   *
+   * @return void
+   */
+  public function __construct()
+  {
+    $this->middleware('auth:api', ['except' => ['login']]);
+  }
+
+  /**
+   * Get a JWT via given credentials.
+   *
+   * @return \Illuminate\Http\JsonResponse
+   */
+  public function login()
+  {
+    \Log::info('test');
+    $credentials = request(['email', 'password']);
+
+    if (!$token = auth()->attempt($credentials)) {
+      return response()->json(['error' => 'Unauthorized'], 401);
     }
 
-    /**
-     * Get a JWT via given credentials.
-     *
-     * @return \Illuminate\Http\JsonResponse
-     */
-    public function login()
-    {
-       \Log::info('test');
-        $credentials = request(['email', 'password']);
+    return $this->respondWithToken($token);
+  }
 
-        if (! $token = auth()->attempt($credentials)) {
-            return response()->json(['error' => 'Unauthorized'], 401);
-        }
+  /**
+   * Get the authenticated User.
+   *
+   * @return \Illuminate\Http\JsonResponse
+   */
+  public function me()
+  {
+    return response()->json(auth()->user());
+  }
 
-        return $this->respondWithToken($token);
-    }
+  /**
+   * Log the user out (Invalidate the token).
+   *
+   * @return \Illuminate\Http\JsonResponse
+   */
+  public function logout()
+  {
+    auth()->logout();
 
-    /**
-     * Get the authenticated User.
-     *
-     * @return \Illuminate\Http\JsonResponse
-     */
-    public function me()
-    {
-        return response()->json(auth()->user());
-    }
+    return response()->json(['message' => 'Successfully logged out']);
+  }
 
-    /**
-     * Log the user out (Invalidate the token).
-     *
-     * @return \Illuminate\Http\JsonResponse
-     */
-    public function logout()
-    {
-        auth()->logout();
-
-        return response()->json(['message' => 'Successfully logged out']);
-    }
-
-    /**
-     * Refresh a token.
-     *
-     * @return \Illuminate\Http\JsonResponse
-     */
-    protected function respondWithToken($token)
-    {
-        return response()->json([
-            'access_token' => $token,
-            'token_type' => 'bearer',
-            'expires_in' => auth("api")->factory()->getTTL() * 60
-        ]);
-    }
+  /**
+   * Refresh a token.
+   *
+   * @return \Illuminate\Http\JsonResponse
+   */
+  protected function respondWithToken($token)
+  {
+    \Log::info($token);
+    return response()->json([
+      // 'token' => $token,
+      'access_token' => $token,
+      'token_type' => 'bearer',
+      'expires_in' => auth("api")->factory()->getTTL() * 60
+    ]);
+  }
 }
